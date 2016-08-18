543ee925f5199b46d2b1d75a4c9cbd9d2708654a
vanstorm9 <antlowhur@yahoo.com>
2016-08-18 15:24:10 -0400

    Added a sample script

--- augmented-reality/stitch.py
+++ augmented-reality/stitch.py
@@ -1,29 +1,95 @@
-# USAGE
-# python stitch.py --first images/bryce_left_01.png --second images/bryce_right_01.png 
-
-# import the necessary packages
 from panorama.panorama import Stitcher
 import imutils
+import numpy as np
+import os
 import cv2
 
-firstImage = 'images/yosemite2.jpg'
-secondImage = 'images/yosemite3.jpg'
+widthResize = 600
+
+def cropFocus(img):
+	subtractThresh = 20
 
 
-# load the two images and resize them to have a width of 400 pixels
-# (for faster processing)
-imageA = cv2.imread(firstImage)
-imageB = cv2.imread(secondImage)
-imageA = imutils.resize(imageA, width=400)
-imageB = imutils.resize(imageB, width=400)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	_, thresh = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+	contours = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	cnt = contours[0]
+	x,y,w,h = cv2.boundingRect(cnt)
+	crop = img[y:y+h,x:x+w-subtractThresh]
+	return crop
 
-# stitch the images together to create a panorama
-stitcher = Stitcher()
-(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+def listDirCreate(root):
+	for imgPath in root:
+		imgPathList.append(imgPath)
+	imgPathList.sort()
+	return imgPathList
 
-# show the images
-cv2.imshow("Image A", imageA)
-cv2.imshow("Image B", imageB)
-cv2.imshow("Keypoint Matches", vis)
+def drawHarris(img):
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	gray = np.float32(gray)
+	dst = cv2.cornerHarris(gray,2,3,0.04)
+
+	#result is dilated for marking the corners, not important
+	dst = cv2.dilate(dst,None)
+
+	# Threshold for an optimal value, it may vary depending on the image.
+	img[dst>0.01*dst.max()]=[0,0,255]
+	return img
+
+
+root_path = 'panorama-input/'
+slash = '/'
+root = os.listdir(root_path)
+
+i = 0
+result = None
+imgPathList = []
+
+# Creating list of paths
+listDirCreate = listDirCreate(root)
+
+print imgPathList
+for fn in imgPathList:
+	print fn
+	if i == 0:
+		# This is our first image
+		mainImage = cv2.imread(root_path + slash + fn)
+		mainImage = imutils.resize(mainImage, width=widthResize)
+		#cv2.imwrite("mainImage.jpg", mainImage)
+		i = i + 1
+		continue
+	else:
+		# We shall combine current image with main image
+		#mainImage = cv2.imread("mainImage.jpg")
+		imageB = cv2.imread(root_path + slash + fn)
+		imageB = imutils.resize(imageB, width=widthResize)
+
+		# stitch the images together to create a panorama
+		stitcher = Stitcher()
+		result =  stitcher.stitch([mainImage, imageB], showMatches=False)
+		mainImage = cropFocus(result)
+
+		# show the images
+		'''	
+		cv2.imshow("Image A", mainImage)
+		cv2.imshow("Image B", imageB)
+		'''
+		'''
+		cv2.imwrite("result.jpg", result)
+
+		cv2.imshow("Result", result)
+		cv2.waitKey(0)
+		'''
+		i = i + 1
+
+
+cv2.imwrite("result.jpg", result)
+
+pointresult = result.copy()
+pointresult = drawHarris(pointresult)
+
 cv2.imshow("Result", result)
+cv2.imshow("Points", pointresult)
+
+
 cv2.waitKey(0)
